0246c9676b8b013df8affd50622eb0b84ad73082
Dario D. Müller <dario-domi@hotmail.de>
2020-12-04 18:45:20 +0100

    added js calc borders aspect-ratio independent

--- custom.js
+++ custom.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
-  console.log('Welcome to Softwerkstatt! :)');
-  var res = $('.bottom').width();
-  $('.bottom').css({
-      'height': res / 1080 * 600  + 'px'
-  });
+	initWebsite();
+	refreshWebsite();
 })
+$(window).resize(() => {
+	refreshWebsite();
+});
+function initWebsite() {
+  console.log('Welcome to Softwerkstatt! :)');
+}
+
+var aspectRatio = -1;
+var borderWidth = 1920;
+var borderheight = 600;
+
+function refreshWebsite() {
+  var newAspectRatio = $(window).width() / $(window).height();
+  if(aspectRatio != newAspectRatio) {
+    aspectRatio = newAspectRatio;
+    var wantAspectRatio = borderWidth / borderheight;
+    var wantHeight = $(window).width() / wantAspectRatio;
+    $('.border').css('height', wantHeight + 'px');
+    $('.border-container:not(footer) .container').css('padding', wantHeight / 2 + 'px 0');
+    $('footer.border-container .container').css('padding-top', wantHeight / 3 + 'px');
+  }
+}
