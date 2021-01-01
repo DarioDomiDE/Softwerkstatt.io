a913d13a575954f4de762b04b0e8f4748ae29588
Dario D. Müller <dario-domi@hotmail.de>
2021-01-01 13:11:54 +0800

    added references page

--- src/App.vue
+++ src/App.vue
@@ -38,33 +38,4 @@ export default {
 </script>
 
 <style>
-h1, h2, h3, h4, h5, h6 {
-  font-weight: bold;
-}
-h1 {
-  font-size: 4.8em;
-  line-height: 1.0;
-}
-  h1 span.smaller {
-    font-size: 0.75em;
-  }
-h2 {
-  font-size: 3.8em;
-}
-h3 {
-  /* font-size: 3.8em; */
-}
-h4 {
-  /* font-size: 3.8em; */
-}
-h5 {
-  /* font-size: 3.8em; */
-}
-h6 {
-  font-size: 2.0em;
-}
-
-h1, h2, h3, h4, h5, h6, p, span {
-  font-family: 'Tw Cen MT', sans-serif;
-}
 </style>
--- src/components/References.vue
+++ src/components/References.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="container h-screen pt-32 pb-32">
-    References
-  </div>
+  <section class="container mx-auto max-w-screen-lg h-screen pt-16 pb-16 px-4 lg:px-0">
+    <h2>Referenzen</h2>
+    <p class="w-full md:max-w-2xl">Ein Dutzend gute Gründe, mit uns zu arbeiten. Dies ist ein Auszug unserer Kunden, bei denen wir stolz sind, sie glücklich gemacht und unterstützt haben zu dürfen.</p>
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+    gap-x-8 sm:gap-x-16 lg:gap-x-32
+    gap-y-8 lg:gap-y-16
+    mt-16 w-full">
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/fernsehlotterie.png" alt="Deutsche Fernsehlotterie" />
+      <img src="./../assets/img/logos/fielmann.png" alt="Fielmann" />
+      <img src="./../assets/img/logos/weischer.png" alt="Weischer Media" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+      <img src="./../assets/img/logos/kalypso.png" alt="Kalypso Media Mobile" />
+    </div>
+  </section>
 </template>
 
 <script>
@@ -10,4 +28,14 @@ export default {
 </script>
 
 <style scoped>
+img {
+  max-height: 60px;
+
+  -webkit-filter: grayscale(100%); /* Ch 23+, Saf 6.0+, BB 10.0+ */
+  filter: grayscale(100%); /* FF 35+ */
+}
+img:hover {
+  -webkit-filter: grayscale(0%); /* Ch 23+, Saf 6.0+, BB 10.0+ */
+  filter: grayscale(0%); /* FF 35+ */
+}
 </style>
