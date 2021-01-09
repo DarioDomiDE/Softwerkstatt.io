9807212a42d295bc152ea8c71e48ec3d2a533e98
Dario D. Müller <dario-domi@hotmail.de>
2021-01-09 14:05:48 +0800

    changed for github pages deployment

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:DarioDomiDE/Softwerkstatt.io.git master:master
+git push -f git@github.com:DarioDomiDE/Softwerkstatt.io.git master:gh-pages
 cd -
