2b3e0bc56e295b24e9727885139690340c2a87d5
Dario D. Müller <dario-domi@hotmail.de>
2021-01-13 14:58:56 +0800

    added Services6 and Intro: each Text and MouseOver effect

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,10 +2,15 @@
   <curved class="gradient min-h-screen">
     <div class="container max-w-screen-xl mx-auto text-white">
       <h1 class="mt-8">„Wir haben eine Vision:<br /><span class="smaller">Unser Handwerk und Ihre Software.“</span></h1>
-      <p class="my-16">Ahoi! Segel setzen! Anker lichten! Auf zu neuen Ufern...<br />
-        Wir entwickeln Software für Ihre digitalen Prozesse und Geschäftsmodelle.<br />
-        Wir sind Macher. Ein Zusammenschluss leidenschaftlicher Softwareentwickler.</p>
-      <CtaButton></CtaButton>
+      <div id="introText" class="my-16">
+        <p class="part1">Ahoi! Segel setzen... Anker lichten! Auf zu neuen Ufern.<br />
+          Wir entwickeln Software für Ihre digitalen Prozesse und Geschäftsmodelle.<br />
+          Wir sind Macher. Ein Zusammenschluss leidenschaftlicher Softwareentwickler.</p>
+        <p class="part2">Ahoi! Segel setzen... Wir sind keine klassische Werdeagentur!<br />
+          Wir sind Techies im Herzen, ein Team von Software-Entwicklern und Freelancern,<br />
+          mit der Mission, gemeinsam unsere Kunden noch besser zu unterstützen.</p>
+      </div>
+      <CtaButton class="block"></CtaButton>
     </div>
   </curved>
 </template>
@@ -41,4 +46,29 @@ h1 span.smaller {
 .gradient {
   background: linear-gradient(45deg, #36c1c7 0%, #67dfc4 90%) !important;
 }
+#introText .part1,
+#introText:hover .part2 {
+  display: block;
+}
+#introText .part2,
+#introText:hover .part1 {
+  display: none;
+}
+
+#introText {
+  border: 1px solid transparent;
+  transition: all 0.6s;
+  display: inline-block;
+}
+
+#introText:hover {
+  border-color: #fff;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 3px;
+  margin: calc(4rem - 10px) 0 calc(4rem - 10px) -20px;
+  padding: 10px 20px;
+}
+#introText .part2 {
+}
+
 </style>
--- src/components/Services6.vue
+++ src/components/Services6.vue
@@ -2,31 +2,31 @@
   <curved class="gradient min-h-screen" v-bind:borderTopGray="true">
     <div class="container max-w-screen-xl text-white px-4 md:px-8 2xl:px-0">
       <h2>Leistungen</h2>
-      <p class="mb-12 w-full md:max-w-4xl">Unsere Domäne, hierbei haben wir Spaß. Wir sind im Herzen Techies. Und unterstützen unsere Kunden in allen Phasen Ihrer Digitalisierung. Eine Auwahl, was uns besonders am Herzen liegt:</p>
+      <p class="mb-12 w-full md:max-w-4xl">Unsere Domäne, hierbei haben wir Spaß. Wir sind im Herzen Techies. Und unterstützen unsere Kunden in allen Phasen ihrer Digitalisierung. Eine Auwahl, was uns besonders am Herzen liegt:</p>
       <div class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-16 gap-y-8">
         <article>
-          <h4>Analyse</h4>
-          <p>Unsere Expertise für neue Blickwinkel: Lösungen für Ideen und Anforderungen konzipieren, denn komplexe Aufgaben sind unsere Leidenschaft.</p>
+          <h4>Strategie</h4>
+          <p>Unsere Expertise für neue Strategien und Blickwinkel: Wir helfen Ihnen, Ideen und Anforderungen zu analyiseren, und Lösungen zu konzipieren, um innovative Produkte zu planen und Chancen zu nutzen.</p>
+        </article>
+        <article>
+          <h4>User-Feedback</h4>
+          <p>Markteinführung radikal beschleunigen und Risiken vermeiden: Feedback-Loops, A/B-Tests und User-driven helfen uns, Ihre Software ganz nach Bedarf und Wünschen Ihrer User und deren Nutzerbedürfnisse auszurichten.</p>
         </article>
         <article>
           <h4>Prototyping</h4>
-          <p>Ihre Ideen sind unsere Baustellen: Prototypen und Mockups helfen der schnellen Validierung neuer Projekte. Unsere Lieblingswörter sind schnelle Markteinführung und Skalierbarkeit.</p>
+          <p>Validierung vom Look &amp; Feel mithilfe von Prototypen und Mockups. Wir unterstützen Sie mit Design Thinking und Design Concepts, um kontinuierlich und ab Tag 1 die User Experience zu verbessern.</p>
         </article>
         <article>
           <h4>Automatisierung</h4>
           <p>Manuelle Prozesse war 2020: Gerne optimieren rund alles (und mehr) rund um CSV/Excel Exporte, FTP-Uploads, Kommunikation zu bestehenden CRM, CMS und ähnlichen Services.</p>
         </article>
         <article>
-          <h4>Userorientiert</h4>
-          <p>Digitalisierung radikal beschleunigen und Risiken vermeiden: Feedback-Loops, A/B-Tests und User-driven für Priorisierung. Moderne digitale Welten ganz nach Bedarf und Wünschen der User.</p>
+          <h4>Programmierung</h4>
+          <p>Das Herztück unseres Daseins: Individual-entwicklung von Apps, Websites, Backend und Datenbanken: Wir lieben Open-Source-Technologien, plattformübergreifende Entwickelung und das Microsoft Ökosystem.</p>
         </article>
         <article>
           <h4>Priorisierung</h4>
-          <p>Effizient vs effektiv, Lieber Schnell am Markt statt perfektes Handwerk. Wir hinterfragen, was ist dringend, aber was wichtig ist. Konzentration auf Core-Features und Kompromisse eingehen.</p>
-        </article>
-        <article>
-          <h4>Programmierung</h4>
-          <p>Das Herztück, warum es uns gibt: Individualentwicklung von Apps, Websites, Backends und Datenbanken: Optimeriung der internen Geschäfts-Prozesse, oder neue Nutzergewinnung durch Neueinführung.</p>
+          <p>Effizient vs effektiv, Lieber Schnell am Markt statt perfektes Handwerk. Wir hinterfragen ständig, was ist <b>dringend</b>, aber was auch <b>wichtig</b> ist. Konzentration auf Core-Features und Kompromisse eingehen.</p>
         </article>
       </div>
       <CtaButton class="mt-8"></CtaButton>
@@ -49,5 +49,16 @@ export default {
 <style scoped>
 .gradient {
   background: linear-gradient(45deg, #168cf8 0%, #59dfff 90%) !important;
+}
+article {
+  border: 1px solid transparent;
+  transition: all 0.6s;
+}
+article:hover {
+  border-color: #fff;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 3px;
+  margin: -10px -20px;
+  padding: 10px 20px;
 }
 </style>
