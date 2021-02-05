5594e26e09d2808b7f14852473eedd79bfd9f8aa
Dario D. Müller <dario-domi@hotmail.de>
2021-02-05 16:04:09 +0800

    added overriding github pages 404 page

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 set -e
 # build
 npm run build
+# use github pages CNAME file
 cp CNAME dist/CNAME
 # navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# allow custom urls by overriding github's 404 page
+cp index.html 404.html
+# commit
 git init
 git add -A
 git commit -m 'deploy'
