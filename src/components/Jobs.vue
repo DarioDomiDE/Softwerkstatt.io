4d1e6f39b6340f3778ef27d350c3996203fb7ba5
Dario D. Müller <dario-domi@hotmail.de>
2021-02-18 22:10:42 +0800

    changed Jobs about-text

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -3,24 +3,36 @@
     <span class="block pre-headline text-center">Mannschaft</span>
     <h1 class="text-center">Jobs als Softwareentwickler</h1>
     <h2>Softwareentwickler in Hamburg</h2>
-    <p>Als <a href="https://www.hamburg.de/stellenangebote-hamburg/4286608/softwareentwickler-hamburg/">Softwareentwickler in Hamburg</a> entwickelst, bugfixst und erstellst Architektur für Web-Applikationen, Tools und Backend-Lösungen. Wir fördern allround-Talente und du arbeitest als <a href="https://de.wikipedia.org/wiki/Softwareentwickler">Entwickler in diversen Rollen</a>, um Programme und Prozesse unserer Kunden zu optimieren und neu zu programmieren. Als branchenübergreifender <a href="https://jaxenter.de/software-consultants-21060">Software-Consultant</a> erwarten dich regelmäßig neue und abwechslungsreiche Aufgaben.</p>
+    <p>Als <a href="https://www.hamburg.de/stellenangebote-hamburg/4286608/softwareentwickler-hamburg/">Softwareentwickler in Hamburg</a> entwickelst und erstellst die Architektur für Web-Applikationen, Tools und Backend-Lösungen. Wir fördern Allround-Talente und Du arbeitest als <a href="https://de.wikipedia.org/wiki/Softwareentwickler">Entwickler in diversen Rollen</a>, um Programme und Prozesse zu optimieren oder neu zu programmieren. Als branchenübergreifender <a href="https://jaxenter.de/software-consultants-21060">Software-Consultant</a> erwarten Dich regelmäßig neue Aufgaben mit abwechslungsreichen Anforderungen.</p>
     <h3>Ahoi Matrosen :)</h3>
-    <p>Wir sind ein Zusammenschluss leidenschaftlicher Entwickler, die es lieben, Software zu entwickeln. Unsere Ziel ist es, benutzerfreundliche Web-Applikationen zu entwickeln, die Mitarbeitern und Endnutzern die Arbeit erleichtern. Um dieses Ziel zu erreichen und um das bestmögliche Produkt zu entwickeln, ist jedes Teammitglied gefragt! Eigenverantwortung und selbständiges Denken ist nicht nur gern gesehen, sondern gehört zu den täglichen Aufgaben.</p>
-    <p>Wir suchen vielversprechende Entwickler/innen und legen Wert auf Charakter-Eigenschaften, Intelligenz und Persönlichkeit anstatt auf Ausbildung und Skills. Wir freuen uns auf Deine Ideen und Fähigkeiten!</p>
-    <p>Unser Büro liegt zentral in der Hamburger Innenstadt. In der direkten Umgebung stehen Dir zahlreiche Möglichkeiten für die Mittagspause und einen Bummel nach der Arbeit zur Verfügung.</p>
-    <h2>Aktuelle Jobs</h2>
+    <p>Wir lieben es, Software zu entwickeln. Unsere Ziel ist es, benutzerfreundliche Web-Applikationen zu entwickeln, die die Arbeit erleichtern. Um dieses Ziel zu erreichen und das bestmögliche Produkt zu erfinden, ist jedes Teammitglied gefragt! Eigenverantwortung und selbständiges Denken ist nicht nur gern gesehen, sondern gehört zu Deinen täglichen Aufgaben. Worauf wartest du? Komm zu uns ins Team!</p>
+    <p>Wir suchen vielversprechende Entwickler/innen und legen Wert auf Charakter-Eigenschaften, Intelligenz und Persönlichkeit anstatt auf Ausbildung und Skills. Wir freuen uns auf Deine Ideen und neue Blickwinkel!</p>
+    <p>Unser Büro liegt zentral in der Hamburger Innenstadt. In der direkten Umgebung stehen Dir zahlreiche Möglichkeiten für die Mittagspause oder einen Bummel nach der Arbeit zur Verfügung.</p>
+    <h2>Jobs to be done</h2>
     <ul class="list">
       <li><router-link to="/jobs/fullstack-entwickler-hamburg">Full-Stack Entwickler/in</router-link></li>
       <li><router-link to="/jobs/dotnet-entwickler-hamburg">.Net Entwickler/in</router-link></li>
     </ul>
-    <h3>Praktika / Werkstudenten</h3>
+    <h3>Praktika</h3>
     <ul class="list">
       <li><router-link to="/jobs">Werkstudent/in in der Software-Entwicklung</router-link></li>
     </ul>
+    <div class="bg-gray-700 -mx-4 lg:mx-0 xl:-mx-32 2xl:-mx-64 text-white my-8 py-8 sm:py-16">
+      <div class="px-4 sm:px-0 md:w-4/5 lg:w-3/5 2xl:w-2/5 mx-auto">
+        <img class="sm:float-left w-2/5 border-4 rounded-full mx-auto" style="border-color: #67dfc4;" src="./../assets/img/about_us.jpg" alt="" />
+        <div class="sm:float-right sm:w-3/5 sm:pl-8">
+          <h3 style="color: #67dfc4;" class="sm:text-6xl">Noch Fragen?</h3>
+          <p><b>Hi, ich bin Dario!</b></p>
+          <p>Ich möchte gerne mit Dir zusammen arbeiten! Wenn Du also noch Fragen oder eine Initiativbewerbung im Gepäck hast, schreib mir gern eine Mail.</p>
+          <p><a href="mailto:moin@softwerkstatt.io">moin@softwerkstatt.io</a></p>
+        </div>
+      </div>
+      <div class="clear-both"></div>
+    </div>
     <h3>Initiativbewerbung</h3>
-    <p class="max-w-2xl">Keine passendender Ausschreibung dabei? Lass uns trotzdem kennen lernen: Schick uns initiativ Deine Bewerbungsunterlagen mit Deiner Wunschposition, Gehaltsvorstellung und möglichem Eintrittstermin.</p>
+    <p>(Noch) nicht das Richtige dabei? Keine Bange: Das muss nichts heißen! Vielleicht kannst du uns trotzdem helfen. Schick uns initiativ Deine Bewerbungsunterlagen zu und lass uns kennen lernen.</p>
     <button class="bg-white rounded shadow-md hover:shadow-none border-2 border-white hover:bg-transparent transform scale-102 transition-all outline-none focus:outline-none px-5 sm:px-8 py-3 sm:py-4 text-md sm:text-xl text-gray-700 mb-8 sm:mb-0 mt-8 bg-gray-100 border-gray-100 hover:border-gray-500" v-on:click="onClick">
-      Bewerbung senden an: moin@softwerkstatt.io
+      Jetzt bewerben
     </button>
     <Borders />
   </div>
@@ -46,7 +58,8 @@ export default {
 p {
   margin: 0.5em 0;
 }
-h2, h3 {
+.container > h2,
+.container > h3 {
   margin-top: 1em;
 }
 </style>
